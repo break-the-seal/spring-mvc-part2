10e6929244ba96ceb9e07f130ee3ea822c823675
beaniejoy <lhb1025@gmail.com>
2022-05-28 22:23:48 +0900

    섹션1 thymeleaf 기본 기능 마무리

---
+++ src/main/java/io/brick/springmvc/TemplateController.kt
@@ -0,0 +1,25 @@
+package io.brick.springmvc
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/template")
+class TemplateController {
+
+    @GetMapping("/fragment")
+    fun template(): String {
+        return "template/fragment/fragmentMain"
+    }
+
+    @GetMapping("/layout")
+    fun layout(): String {
+        return "template/layout/layoutMain"
+    }
+
+    @GetMapping("/layoutExtend")
+    fun layoutExtend(): String {
+        return "template/layoutExtend/layoutExtendMain"
+    }
+}
