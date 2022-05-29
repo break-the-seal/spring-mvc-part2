2358465e11cb4f7e44a67bd1e4bd7915b37770c9
beaniejoy <lhb1025@gmail.com>
2022-05-30 01:08:31 +0900

    섹션3 메시지, 국제화 메시지 소스 설정까지

--- src/main/java/io/brick/springmvc/SpringMvcPart2Application.kt
+++ src/main/java/io/brick/springmvc/SpringMvcPart2Application.kt
@@ -5,11 +5,13 @@ import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class SpringMvcPart2Application {
+
+    // spring에서 MessageSource를 자동으로 등록을 해준다.
 /*
     @Bean
     fun messageSource(): MessageSource {
         return ResourceBundleMessageSource().apply {
-            setBasenames("messages", "errors")
+            setBasenames("messages", "errors")  // messages.properties, errors.properties 인식
             setDefaultEncoding("utf-8")
         }
     }
