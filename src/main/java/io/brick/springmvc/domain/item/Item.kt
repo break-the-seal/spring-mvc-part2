caea55d88f4840905a4e6d6204bdeefeb3741d18
beaniejoy <lhb1025@gmail.com>
2022-05-30 00:19:01 +0900

    섹션2 스프링 통합과 폼 마무리

--- src/main/java/io/brick/springmvc/domain/item/Item.kt
+++ src/main/java/io/brick/springmvc/domain/item/Item.kt
@@ -1,19 +1,12 @@
 package io.brick.springmvc.domain.item
 
-class Item constructor() {
-    var id: Long = 0L
-    var itemName: String? = null
-    var price: Int? = null
-    var quantity: Int? = null
-
-    var open: Boolean? = null
-    var regions: List<String> = listOf()
-    var itemType: ItemType? = null
+data class Item(
+    var id: Long = 0L,
+    var itemName: String? = null,
+    var price: Int? = null,
+    var quantity: Int? = null,
+    var open: Boolean? = null,
+    var regions: List<String> = listOf(),
+    var itemType: ItemType? = null,
     var deliveryCode: String? = null
-
-    constructor(itemName: String, price: Int, quantity: Int): this() {
-        this.itemName = itemName
-        this.price = price
-        this.quantity = quantity
-    }
-}
+)
