1daf312fc9e1c307d751d4356bcff6b936f6cf9d
verre-u <verre.u@kakaoenterprise.com>
2022-06-03 16:08:37 +0900

    validation(v1) 프로젝트 설정

--- src/main/java/io/brick/springmvc/SpringMvcPart2Application.kt
+++ src/main/java/io/brick/springmvc/SpringMvcPart2Application.kt
@@ -7,17 +7,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.support.ResourceBundleMessageSource
 
 @SpringBootApplication
-class SpringMvcPart2Application {
-
-//    @Bean
-//    fun messageSource(): MessageSource {
-//        val messageSource = ResourceBundleMessageSource()
-//        messageSource.setBasenames("messages", "errors")  // messages.properties, errors.properties 인식
-//        messageSource.setDefaultEncoding("utf-8")
-//
-//        return messageSource
-//    }
-}
+class SpringMvcPart2Application
 
 fun main(args: Array<String>) {
     runApplication<SpringMvcPart2Application>(*args)
