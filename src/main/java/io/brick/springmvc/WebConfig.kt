5babfd111d75ef58da987e8e682d653b13f1c889
verre-u <verre.u@kakaoenterprise.com>
2022-06-20 16:42:54 +0900

    로그인 처리 - spring 인터셉터(요청로그, 인증체크)

--- src/main/java/io/brick/springmvc/WebConfig.kt
+++ src/main/java/io/brick/springmvc/WebConfig.kt
@@ -2,18 +2,34 @@ package io.brick.springmvc
 
 import io.brick.springmvc.web.filter.LogFilter
 import io.brick.springmvc.web.filter.LoginCheckFilter
+import io.brick.springmvc.web.interceptor.LogInterceptor
+import io.brick.springmvc.web.interceptor.LoginCheckInterceptor
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import javax.servlet.Filter
 
 @Configuration
-class WebConfig {
+class WebConfig : WebMvcConfigurer {
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(LogInterceptor())
+            .order(1)
+            .addPathPatterns("/**")
+            .excludePathPatterns("/css/**", "/*.ico", "/error")
+
+        registry.addInterceptor(LoginCheckInterceptor())
+            .order(2)
+            .addPathPatterns("/**")
+            .excludePathPatterns("/", "/members/add", "/login", "/logout", "/css/**", "/*.ico", "/error")
+    }
 
     /**
      * @return FilterRegistrationBean -> 스프링 부트 내부에 WAS를 띄울 때 Filter 등록해준다.
      */
-    @Bean
+//    @Bean
     fun logFilter(): FilterRegistrationBean<*> {
         val filterRegistrationBean: FilterRegistrationBean<Filter> = FilterRegistrationBean()
         filterRegistrationBean.filter = LogFilter()
@@ -23,7 +39,7 @@ class WebConfig {
         return filterRegistrationBean
     }
 
-    @Bean
+//    @Bean
     fun loginCheckFilter(): FilterRegistrationBean<*> {
         val filterRegistrationBean: FilterRegistrationBean<Filter> = FilterRegistrationBean()
         filterRegistrationBean.filter = LoginCheckFilter()
---
+++ src/main/java/io/brick/springmvc/web/interceptor/LoginCheckInterceptor.kt
@@ -0,0 +1,30 @@
+package io.brick.springmvc.web.interceptor
+
+import io.brick.springmvc.web.SessionConstant
+import mu.KLogging
+import org.springframework.web.servlet.HandlerInterceptor
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class LoginCheckInterceptor : HandlerInterceptor{
+
+    companion object : KLogging()
+
+    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+        val requestURI = request.requestURI
+
+        logger.info { "인증 체크 인터셉터 실행 ${requestURI}" }
+
+        logger.info { "인증 체크 로직 실행 ${requestURI}" }
+        val session = request.getSession(false)
+
+        if (session?.getAttribute(SessionConstant.LOGIN_MEMBER) == null) {
+            logger.info { "미인증 사용자 요청 ${requestURI}" }
+            // 로그인으로 redirect
+            response.sendRedirect("/login?redirectURL=${requestURI}")
+            return false
+        }
+
+        return true
+    }
+}
