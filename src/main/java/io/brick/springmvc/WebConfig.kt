c4c4f965c181b0f97034a1e92a44e01896802e10
verre-u <verre.u@kakaoenterprise.com>
2022-06-22 16:44:48 +0900

    서블릿 예외처리 - filter, interceptor

---
+++ src/main/java/io/brick/springmvc/WebConfig.kt
@@ -0,0 +1,37 @@
+package io.brick.springmvc
+
+import io.brick.springmvc.filter.LogFilter
+import io.brick.springmvc.interceptor.LogInterceptor
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import javax.servlet.DispatcherType
+import javax.servlet.Filter
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(LogInterceptor())
+            .order(1)
+            .addPathPatterns("/**")
+            // filter 같이 dispatcherType으로 설정 불가능
+            // exclude할 path로 지정
+            .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**")
+    }
+
+//    @Bean
+    fun logFilter(): FilterRegistrationBean<*> {
+        val filterRegistrationBean = FilterRegistrationBean<Filter>()
+        filterRegistrationBean.filter = LogFilter()
+        filterRegistrationBean.order = 1
+        filterRegistrationBean.addUrlPatterns("/*")
+
+        // 여기서 filter에 DispatcherType 적용 가능(어떤 타입에 필터를 적용할 것인지 설정)
+        // 기본값은 "REQUEST"만 설정
+         filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR)
+
+        return filterRegistrationBean
+    }
+}
