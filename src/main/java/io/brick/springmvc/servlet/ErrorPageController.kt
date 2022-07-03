216b655d3a53e2671724adc84b6d2e16c59c1cf6
verre-u <verre.u@kakaoenterprise.com>
2022-07-03 20:13:03 +0900

    API 예외처리 초기 설정

--- src/main/java/io/brick/springmvc/servlet/ErrorPageController.kt
+++ src/main/java/io/brick/springmvc/servlet/ErrorPageController.kt
@@ -1,8 +1,12 @@
 package io.brick.springmvc.servlet
 
 import mu.KLogging
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import javax.servlet.RequestDispatcher
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -31,6 +35,22 @@ class ErrorPageController {
         logger.info { "errorPage 500" }
         printErrorInfo(request)
         return "error-page/500"
+    }
+
+    @GetMapping("/error-page/500", produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun errorPage500Api(
+        request: HttpServletRequest,
+        response: HttpServletResponse
+    ): ResponseEntity<Map<String, Any?>> {
+        logger.info { "API errorPage 500" }
+
+        val result = mutableMapOf<String, Any?>()
+        val ex = request.getAttribute(ERROR_EXCEPTION) as? Exception
+        result["status"] = request.getAttribute(ERROR_STATUS_CODE)
+        result["message"] = ex?.message
+
+        val statusCode = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE) as? Int
+        return ResponseEntity(result, HttpStatus.valueOf(statusCode!!))
     }
 
     private fun printErrorInfo(request: HttpServletRequest) {
